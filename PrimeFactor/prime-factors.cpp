d577175091330f33410fc6bf4788d2be2c731cdf
may6ix.song <smh92996275@gmail.com>
2023-07-05 15:37:21 +0900

    되돌리기

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -1,19 +1,5 @@
-#include <vector>
-using namespace std;
-
 class PrimeFactor
 {
 public:
-	vector<int> of(int number) {
-		vector<int> result = {};
 
-		for (int divisor = 2; number > 1; divisor++) {
-			while (number % divisor == 2) {
-				result.push_back(divisor);
-				number /= divisor;
-			}
-		}
-
-		return result;
-	}
 };
